37962cd1b2aa4aa24479e5b0db29120268dd1564
Chan Kuang Wen <kwen1510@hotmail.com>
2022-12-23 00:39:12 +0800

    reverted to without Gsheets

--- index.py
+++ index.py
@@ -2,31 +2,31 @@ from flask import Flask, render_template, request, jsonify
 import os
 import json
 import requests
-from gspread_pandas import Spread, Client
-from google.oauth2 import service_account
-from oauth2client.service_account import ServiceAccountCredentials
-from dotenv import load_dotenv
+# from gspread_pandas import Spread, Client
+# from google.oauth2 import service_account
+# from oauth2client.service_account import ServiceAccountCredentials
+# from dotenv import load_dotenv
 
 ### Load items to update Google Sheets ### 
 
-load_dotenv()  # take environment variables from .env.
+# load_dotenv()  # take environment variables from .env.
 
-json_credentials = json.loads(os.getenv("credentials"))
+# json_credentials = json.loads(os.getenv("credentials"))
 
-# Create a Google Authentication connection object
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
+# # Create a Google Authentication connection object
+# scope = ['https://spreadsheets.google.com/feeds',
+#          'https://www.googleapis.com/auth/drive']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_dict(
-                json_credentials, scopes = scope)
-client = Client(scope=scope,creds=credentials)
-spreadsheetname = "Semantic Search Tool Responses"
-spread = Spread(spreadsheetname,client = client) 
+# credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+#                 json_credentials, scopes = scope)
+# client = Client(scope=scope,creds=credentials)
+# spreadsheetname = "Semantic Search Tool Responses"
+# spread = Spread(spreadsheetname,client = client) 
 
-sh = client.open(spreadsheetname)
+# sh = client.open(spreadsheetname)
 
-# Get sheet 1
-sheet_1 = sh.sheet1
+# # Get sheet 1
+# sheet_1 = sh.sheet1
 
 ### Flask Code ###
 
@@ -55,6 +55,6 @@ def testpost():
         array_content = json_data[key]
         responses_array.append(array_content)
 
-    sheet_1.append_row(responeses_array)
+    # sheet_1.append_row(responeses_array)
         
     return render_template('results.html', responses=responses_array, query=query)
